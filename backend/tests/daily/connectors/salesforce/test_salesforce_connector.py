0adff1e5efba2d2138df96a9990695774507338a
Skylar Kesselring <skylarkesselring@gmail.com>
2025-01-05 20:42:41 -0500

    Add daily salesforce test

--- backend/tests/daily/connectors/salesforce/test_salesforce_connector.py
+++ backend/tests/daily/connectors/salesforce/test_salesforce_connector.py
@@ -1,8 +1,19 @@
+import json
 import os
+import time
+from pathlib import Path
 
 import pytest
 
+from onyx.configs.constants import DocumentSource
+from onyx.connectors.models import Document
 from onyx.connectors.salesforce.connector import SalesforceConnector
+
+
+def load_test_data(file_name: str = "test_salesforce_data.json") -> dict[str, dict]:
+    current_dir = Path(__file__).parent
+    with open(current_dir / file_name, "r") as f:
+        return json.load(f)
 
 
 @pytest.fixture
@@ -18,6 +29,31 @@ def salesforce_connector() -> SalesforceConnector:
         }
     )
     return connector
+
+
+# TODO: make the credentials not expire
+@pytest.mark.xfail(
+    reason=(
+        "Credentials change over time, so this test will fail if run when "
+        "the credentials expire."
+    )
+)
+def test_salesforce_connector_basic(salesforce_connector: SalesforceConnector) -> None:
+    test_data = load_test_data()
+    target_test_doc: Document | None = None
+    all_docs: list[Document] = []
+    for doc_batch in salesforce_connector.poll_source(0, time.time()):
+        for doc in doc_batch:
+            all_docs.append(doc)
+            if doc.id == test_data["id"]:
+                target_test_doc = doc
+
+    assert len(all_docs) == 6
+    assert target_test_doc is not None
+
+    test_data = load_test_data()
+    assert target_test_doc.source == DocumentSource.SALESFORCE
+    assert target_test_doc.semantic_identifier == "Unknown Object"
 
 
 # TODO: make the credentials not expire
